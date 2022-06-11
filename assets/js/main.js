a9e25f145964d07162e6fe2b6cb26154fd3d7aa3
JB <jeanbernard_doffemont@yahoo.fr>
2022-06-11 16:34:51 +0200

    commit showFeatures + showCompagny

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,19 @@
 const menuBtn = document.querySelector(".header__navMenuBtn");
 const sideNav = document.querySelector(".header__navBtnBurger");
 const dropdown = document.querySelector(".dropdown");
+const dropdownFeatures = document.querySelector(".dropdownFeatures");
+const dropdownCompagny = document.querySelector(".dropdownCompagny");
 
 let menuOpen = false;
 
 function showMenu() {
   dropdown.classList.toggle("show");
+}
+function showFeatures() {
+  dropdownFeatures.classList.toggle("show");
+}
+function showCompagny() {
+  dropdownCompagny.classList.toggle("show");
 }
 
 menuBtn.addEventListener("click", () => {
